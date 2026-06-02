5d3e160341ff31f553c7f7b6bce5d44171668ddf
unknown <mbsoares.infor@gmail.com>
2026-06-01 22:07:24 -0300

    correcao das libs

--- app/src/main/java/com/example/labremotoclp/RetrofitClient.java
+++ app/src/main/java/com/example/labremotoclp/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://10.75.255.166:8080/";
+    private static final String BASE_URL = "https://sewing-part-elect-changing.trycloudflare.com/";
             //"http://orientation-mardi-packages-shipped.trycloudflare.com/";
     private static ApiService apiService;
 
